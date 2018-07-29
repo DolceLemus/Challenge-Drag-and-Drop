0f423b224f84701086e70211bafc2971ac0065ee
DolceLemus <dulce.lemus.lara@gmail.com>
2018-07-29 15:53:29 -0500

    se ajustan correctamente las imagenes a sus contenedores

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 "use strict";
-let id ="";
-let id2 =""; 
 const time = () => {
     let today = new Date();
     let hour = today.getHours();
     //se añade un operador ternario para que se muestre un cero cuando los minutos sean menor a 10
-    let minutes = (today.getMinutes()<10? '0' + today.getMinutes() : today.getMinutes()); 
+    let minutes = (today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes());
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minutes").innerHTML = minutes;
 }
@@ -15,34 +13,38 @@ const batteryLevel = () => {
     //se utiliza promesa para seguir con el funcionamiento una vez obtenido el dato con getBattery();
     //getBattery() es obsoleto, solo funciona en algunos navegadores, es por eso que se agrega un if;
     let battery = navigator.getBattery().then(function (battery) {
-        let level = battery.level.toFixed(0) * 100 + "%";
+        let level = battery.level * 100;
+        let levelPerc = level.toFixed(0) + "%";
         let levelNumber = battery.level;
         if (battery === NaN || battery === null) {
             document.getElementById("level").textContent = "";
         } else {
-            document.getElementById("level").textContent = level;
+            document.getElementById("level").textContent = levelPerc;
             // se le añade el valor de levelNumber en una escala a 1
-            document.getElementById("battery").setAttribute('value',levelNumber);
+            document.getElementById("battery").setAttribute('value', levelNumber);
         }
     });
 }
 
-// box1
-const allowDrop = (event) => {
-    event.preventDefault();
+const allowDrop = (ev) => {
+    ev.preventDefault();
 }
 
-const drop = (event) => {
-    event.target.append(document.getElementById(id));
+const dragStart = (ev) => {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
-const dragStart = (event) =>  {
-    id = event.target.id;
+const drop = (ev) => {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    // explanation : ev.target.appendChild(document.getElementById(data)); ev.target fue reemplazado por this para que no fueda posible hacer un drop dentro de otra imagen y esta desaparezca
+    this.appendChild(document.getElementById(data));
+    // Utilicé JQuery por metodos prácticos
+    $("[id*=element]").addClass("dropImage");
+
 }
 
-
-
-
+// --------------------------------------------------------
 
 batteryLevel();
 time();
